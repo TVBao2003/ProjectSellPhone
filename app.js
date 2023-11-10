9b46a11976d29fd1cfffa923db49fa0c19daf8ed
Trương Văn Bảo <truongvanbao2035@gmail.com>
2023-11-10 13:56:27 +0700

    add new

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var changeColor = '#9d9d9d';
 
 /*--------design item produces--------------- */
 var elementsProduceAct = document.querySelectorAll(".produce_activity");
-var listDataProducts = [];
+var listDataProductsAddCart = [];
 
 elementsProduceAct.forEach((e, index)=>{
     var heartBlock = e.querySelector(".heart_item");
@@ -43,19 +43,16 @@ elementsProduceAct.forEach((e, index)=>{
             'Price' : priceProduct
         }
 
-       listDataProducts.push(dataProduct);
+       listDataProductsAddCart.push(dataProduct);
     }
 })
 
 /*---------------------design bar------------------- */
 //----elements of nav-----------
 var listItems = document.querySelectorAll('.list-items');
-var listProductsExist = new Map();
-var arrayName = [];
+var isOpen = false;
 
 listItems.forEach((item, index)=>{
-    var isOpen = false;
-
     //------methods change modal------------
     function clickClose(e){
         isOpen = false;
@@ -87,166 +84,170 @@ listItems.forEach((item, index)=>{
         element.classList.add('change_click-item');
     }
 
+    //----Primary methods-------
 
+    function openModal(check, element, modalElement, linkElement){
+        if(!check){
+            clickOffClose(modalElement);             
+            clickOpen(modalElement);
+            changeOpen(element, linkElement);
+        }
+    }
+
+    function closeModal(check, element, modalElement, linkElement){
+        if(check){
+            clickOffOpen(modalElement);
+            clickClose(modalElement);
+            changeClose(element, linkElement);
+        }
+    }
+
+    function addDataToModal(bodyModal, dataName, dataPrice, dataImg){
+        var containProductSaved = document.createElement('div');
+        var containButtonsBuyOrDelete = document.createElement("div");
+        var informationProductSaved = document.createElement('div');
+        //-----design CSS div tag------
+            
+        containProductSaved.classList.add('dataProducts_contain');
+        informationProductSaved.classList.add('dataProducts-SpaceBetween');
+        informationProductSaved.classList.add('dataProducts_style');
+        containButtonsBuyOrDelete.classList.add('dataProducts-SpaceBetween')
+        //------create span tag-------
+        var spanElementName = document.createElement('span');
+        var spanElementPrice = document.createElement('span');
+                    
+        //------add content and design CSS----------
+        spanElementName.innerText = dataName;
+        spanElementPrice.innerText = dataPrice;
+        spanElementName.style.marginTop = '5px';
+        spanElementPrice.style.marginTop = '5px';
+
+        //---create image tag-----------
+        var imageElement = document.createElement('img');
+                    
+        //-------add src and design CSS--------
+        imageElement.src = dataImg;
+        imageElement.style.width = '30px';
+        imageElement.style.height = '30px';
+
+        var btnPopProduct= document.createElement('button');
+        var btnBuyProduct= document.createElement('button');
+
+        btnPopProduct.classList.add("dataProducts_button--pop");
+        btnBuyProduct.classList.add("dataProducts_button--buy");
+                 
+        btnBuyProduct.innerHTML = "<i class='bx bx-check'></i>";
+        btnPopProduct.innerHTML = "<i class='bx bx-x'></i>";
+
+        //-----add span and image elements in div tag-------
+        informationProductSaved.appendChild(imageElement);
+        informationProductSaved.appendChild(spanElementName);
+        informationProductSaved.appendChild(spanElementPrice);
+
+        containButtonsBuyOrDelete.appendChild(btnBuyProduct);
+        containButtonsBuyOrDelete.appendChild(btnPopProduct);
+
+        containProductSaved.appendChild(informationProductSaved);
+        containProductSaved.appendChild(containButtonsBuyOrDelete);
+        bodyModal.appendChild(containProductSaved);
+    }
+
+
+//------Primary Part--------------------
     if(index === 0){
         isOpen = false;
         var aElement = item.querySelector('a');
         var elementSearchModal = document.querySelector(".modal_search");
-        var btnClose = elementSearchModal.querySelector(".button--close");
-        var inputSearchElementModal = elementSearchModal.querySelector(".input_search_modal");
-        
-        
-        // elementSearchModal.style.display = 'none';
+        var btnClose = elementSearchModal.querySelector(".button--close");     
+
         item.onclick = () =>{
-            if(!isOpen){
-                clickOffClose(elementSearchModal);             
-                clickOpen(elementSearchModal);
-                changeOpen(item, aElement);
-            }
+           openModal(isOpen, item, elementSearchModal, aElement);
         }
 
         btnClose.onclick = ()=>{
-            if(isOpen){
-                clickOffOpen(elementSearchModal);
-                changeClose(item, aElement)
-            }
+           closeModal(isOpen, item, elementSearchModal, aElement)
         }
-
     }
+
+
     //--------is cart item------
     if(index === 3){
+        //---element base
         isOpen = false;
         var aElement = item.querySelector('a');
+        //---element sort data
+        var listProductsExist = new Map();
+        var arrayName = [];
         //---elements of modal cart----------
         var elementCartModal = document.querySelector(".modal_cart");
         var iconClose = elementCartModal.querySelector("i");
         var btnClose = elementCartModal.querySelector(".button--close");
-        var bodyModal = elementCartModal.querySelector('.modal_body');
+        var bodyCartModal = elementCartModal.querySelector('.modal_cart--body');
         
         //-------click to Close----------
         iconClose.onclick = () =>{
-            if(isOpen){
-                clickOffOpen(elementCartModal);
-                changeClose(item, aElement);
-                clickClose(elementCartModal);
-            }
+            closeModal(isOpen, item, elementCartModal, aElement);
         }
-        
+
         btnClose.onclick = ()=>{
-            if(isOpen){
-                clickOffOpen(elementCartModal);
-                changeClose(item, aElement);
-                clickClose(elementCartModal);
-            }
+            closeModal(isOpen, item, elementCartModal, aElement);
         }
         
         elementCartModal.onclick = (e)=>{
-            if(e.target === e.currentTarget){
-                if(isOpen){
-                    clickOffOpen(elementCartModal);
-                    changeClose(item, aElement);
-                    clickClose(elementCartModal);
-                }
-            }
+            if(e.target === e.currentTarget) closeModal(isOpen, item, elementCartModal, aElement);
         }
-
         //-------Click to Open------------
         item.onclick = ()=>{
-            if(!isOpen){
-                clickOffClose(elementCartModal);
-                clickOpen(elementCartModal);
-                changeOpen(item, aElement);
-            }
-
-            //-----------update new data---------- 
-            if(listDataProducts.length === 0 && listProductsExist.size === 0 ){
-                alert("There is no any products")
-            }else{
+            openModal(isOpen, item, elementCartModal, aElement);
+             //-----------update new data---------- 
+            if(listDataProductsAddCart.length === 0 && listProductsExist.size === 0 ) alert("There is no any products")           
+            else{
                 const indexFirstElement = 0;
-                while(indexFirstElement < listDataProducts.length){
-                    if(arrayName.includes(listDataProducts[indexFirstElement].Name)){
-                        listDataProducts.shift();
+                while(indexFirstElement < listDataProductsAddCart.length){                       
+                    if(arrayName.includes(listDataProductsAddCart[indexFirstElement].Name)){
+                        listDataProductsAddCart.shift();
                         continue;
                     }
-                    listProductsExist.set(listDataProducts[indexFirstElement].Name, listDataProducts[indexFirstElement].Id);
-                    arrayName.push(listDataProducts[indexFirstElement].Name);
-                 
-                    //-----create div tag------
-                    var containProductSaved = document.createElement('div');
-                    var informationProductSaved = document.createElement('div');
-                    var containButtonsBuyOrDelete = document.createElement("div");
-                    //-----design CSS div tag------
-               
-                    containProductSaved.classList.add('dataProducts_contain');
-                    informationProductSaved.classList.add('dataProducts-SpaceBetween');
-                    informationProductSaved.classList.add('dataProducts_style');
-                    containButtonsBuyOrDelete.classList.add('dataProducts-SpaceBetween')
-                    //------create span tag-------
-                    var spanElementName = document.createElement('span');
-                    var spanElementPrice = document.createElement('span');
-
-                    //------add content and design CSS----------
-                    spanElementName.innerText = listDataProducts[indexFirstElement].Name;
-                    spanElementPrice.innerText = listDataProducts[indexFirstElement].Price;
-                    spanElementName.style.marginTop = '5px';
-                    spanElementPrice.style.marginTop = '5px';
-
-                    //---create image tag-----------
-                    var imageElement = document.createElement('img');
-
-                    //-------add src and design CSS--------
-                    imageElement.src = listDataProducts[indexFirstElement].Image;
-                    imageElement.style.width = '30px';
-                    imageElement.style.height = '30px';
-
-                    var btnPopProduct= document.createElement('button');
-                    var btnBuyProduct= document.createElement('button');
-
-                    btnPopProduct.classList.add("dataProducts_button--pop");
-                    btnBuyProduct.classList.add("dataProducts_button--buy");
-                 
-                    btnBuyProduct.innerHTML = "<i class='bx bx-check'></i>";
-                    btnPopProduct.innerHTML = "<i class='bx bx-x'></i>";
-
-                    //-----add span and image elements in div tag-------
-                    informationProductSaved.appendChild(imageElement);
-                    informationProductSaved.appendChild(spanElementName);
-                    informationProductSaved.appendChild(spanElementPrice);
-
-                    containButtonsBuyOrDelete.appendChild(btnBuyProduct);
-                    containButtonsBuyOrDelete.appendChild(btnPopProduct);
-
-                    containProductSaved.appendChild(informationProductSaved);
-                    containProductSaved.appendChild(containButtonsBuyOrDelete);
-                    bodyModal.appendChild(containProductSaved);
                     
-                    listDataProducts.shift();
-                }
-            }
-           
+                    //-----add data to bodyModal------
+                    let dataId = listDataProductsAddCart[indexFirstElement].Id
+                    let dataName = listDataProductsAddCart[indexFirstElement].Name;
+                    let dataPrice = listDataProductsAddCart[indexFirstElement].Price;
+                    let dataImg = listDataProductsAddCart[indexFirstElement].Image;
 
+                    listProductsExist.set(dataName, dataId);
+
+                    arrayName.push(dataName);
+                    
+                    addDataToModal(bodyCartModal, dataName, dataPrice, dataImg);
+
+                    listDataProductsAddCart.shift();
+                }
+            }           
             //---------delete Products saved out cart---------------
             var deleteProductsSaved = document.querySelectorAll('.dataProducts_button--pop');
             deleteProductsSaved.forEach((element)=>{
-               element.onclick = ()=>{
+               element.onclick = ()=>{             
                     var nameOfProduct = element.parentElement.parentElement.querySelector("span").textContent;
                     var idOfProduct = listProductsExist.get(nameOfProduct);
                     var indexOfProduct = arrayName.indexOf(nameOfProduct);
-
+        
                     arrayName.splice(indexOfProduct, 1);
                     listProductsExist.delete(nameOfProduct);
-
+        
                     //------change check icon to cart icon
                     elementsProduceAct[idOfProduct].querySelector(".button_save").innerHTML = "<i class='bx bx-cart-alt'></i>";
-
+        
                     //--------pop product out cart
-                    bodyModal.removeChild(element.parentElement.parentElement)
+                    bodyCartModal.removeChild(element.parentElement.parentElement)
                    
                 }
             })
         }
     }
 })
+
+
 //--------------------Modal Menu responsive-------------------
 var responsiveMenu = document.querySelector(".responsive_nav_menu");
 var modalMenu = document.querySelector("#nav_menu");
