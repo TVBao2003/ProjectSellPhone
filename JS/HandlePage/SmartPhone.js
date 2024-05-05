0377f32e98f9fa3599a6f4e4de8f313e750d1aef
Trương Văn Bảo <truongvanbao2035@gmail.com>
2024-05-05 22:22:56 +0700

    add data base

--- JS/HandlePage/SmartPhone.js
+++ JS/HandlePage/SmartPhone.js
@@ -6,6 +6,9 @@ var samsungContainer = document.querySelector('.list_samsung');
 var iphoneContainer = document.querySelector('.list_iphone');
 var listSamSung = [];
 var listIPhone = [];
+
+
+
 //------ GET DATA ------
 for(let i=0; i<products.length; i++){
     if(products[i].name.toLowerCase().includes('samsung')) listSamSung.push(products[i]);
@@ -15,35 +18,16 @@ for(let i=0; i<products.length; i++){
 //------ Render Data ----
 renderData(listSamSung, samsungContainer);
 renderData(listIPhone, iphoneContainer);
+// renderData(listMac, macContainer);
 var listDataProductsAddCart = [];
 handleButtonCard(listDataProductsAddCart);
 
 var btnLeftSamSung = document.querySelector(".btn_left--samsung");
 var btnRightSamSung = document.querySelector(".btn_right--samsung");
-var btnLeftIPhone = document.querySelector(".btn_left--iphone");
-var btnRightIPhone = document.querySelector(".btn_right--iphone");
-
-
-
 handleSlide(btnLeftSamSung, btnRightSamSung);
 
-handleSlide(btnLeftIPhone, btnRightIPhone);
-// listBtnLeft.forEach(btn => {
-//     let blockDisplay = btn.previousElementSibling;
-//     btn.onclick = ()=>{
-//         if(currentWidth !== 0){
-//             currentWidth += 220
-//             blockDisplay.style.transform = `translateX(${currentWidth}px)`;
-//         }
-//     }
-// })
 
-// listBtnRight.forEach(btn => {
-//     let blockDisplay = btn.previousElementSibling;
-//     btn.onclick = ()=>{
-//         if(currentWidth > ((blockDisplay.offsetWidth - 1100)*-1) ){
-//             currentWidth -= 220;
-//             blockDisplay.style.transform = `translateX(${currentWidth}px)`;  
-//         }
-//     }
-// })
+
+var btnLeftIPhone = document.querySelector(".btn_left--iphone");
+var btnRightIPhone = document.querySelector(".btn_right--iphone");
+handleSlide(btnLeftIPhone, btnRightIPhone);
